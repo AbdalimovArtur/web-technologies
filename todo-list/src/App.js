21d96b79a29e67243f77f851a33c9b79500b5e28
AbdalimovArtur <a.abdalimov.97@gmail.com>
2018-01-25 01:07:59 +0600

    Todo app added

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,21 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import List from './List'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoList: [],
+            inputValue: '',
+        }
+    }
+
+    onChange = (event) => {
+        this.setState({inputValue: event.target.value})
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            todoList: [...this.state.todoList, {
+                'todo': this.state.inputValue,
+                'disabled': false,
+                'timestamp' : (new Date()).toString()
+            }],
+            inputValue: '',
+        });
+    };
+
+    deleteTodo = (index) => {
+        let s = this.getTodoList();
+        s.splice(index, 1);
+        this.setState({
+            todoList: s,
+        });
+    };
+
+    disableTodo = (index) => {
+        let s = this.getTodoList();
+        s[index].disabled = !s[index].disabled;
+        this.setState({
+            todoList: s,
+        });
+    };
+
+    getTodoList = () => {
+        let active = this.state.todoList.slice()
+            .filter(x => !x.disabled)
+            .sort((x, y) => new Date(x.timestamp) > new Date(y.timestamp));
+        let deactive = this.state.todoList.slice()
+            .filter(x => x.disabled)
+            .sort((x, y) => new Date(x.timestamp) > new Date(y.timestamp));
+        return [...active, ...deactive];
+    };
+
+    render() {
+        return (
+            <div>
+                <form className="App" onSubmit={this.onSubmit}>
+                    <input value={this.state.inputValue} onChange={this.onChange}/>
+                    <button>Submit</button>
+                </form>
+                <List todoList={this.getTodoList} deleteTodo={this.deleteTodo} disableTodo={this.disableTodo}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ todo-list/src/List.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class List extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            enabled: true
+        }
+    }
+
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.todoList().map((todo, index) =>
+                        <li key={index}>
+                            {todo.todo}
+                            {todo.disabled ? " -> Disabled" : "-> !Disabled"}
+                            {todo.timestamp}
+                            <button onClick={() => this.props.deleteTodo(index)}>Delete</button>
+                            <button onClick={() => this.props.disableTodo(index)}>Disable</button>
+                        </li>
+                    )
+                }
+            </ul>
+        );
+    };
+}
+
+export default List;
