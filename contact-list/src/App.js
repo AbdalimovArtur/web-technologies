c7e6e049cfd0525c6c5f37e271c111bcf6591660
AbdalimovArtur <a.abdalimov.97@gmail.com>
2018-02-01 10:33:20 +0600

    Update Todo now in own container

--- contact-list/src/App.js
+++ contact-list/src/App.js
@@ -1,56 +1,18 @@
 import React, {Component} from 'react';
-import ContactList from './components/Contact/List/ContactList'
-import InputHandler from './components/Input/Input/index'
-import SearchHandler from './components/Input/Search/index'
-import 'bootstrap/dist/css/bootstrap.css'
 import Header from "./components/Header";
+import TodoContainer from "./components/Todo/Container";
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            contacts: [],
-            searchValue: ''
-        }
     }
-
-    onAddClick = (inputValue) => {
-        this.setState({
-            contacts: [...this.state.contacts,
-                {
-                    'firstName' : inputValue.firstName,
-                    'lastName' : inputValue.lastName,
-                    'contactNumber': inputValue.contactNumber
-                }
-            ]
-        })
-    };
-
-    getContacts = () => {
-        console.log(this.state.searchValue);
-        let p = this.state.contacts.slice();
-        p = p.filter(x => (this.state.searchValue === '' ? true : (x.firstName.startsWith(this.state.searchValue) ||
-            x.lastName.startsWith(this.state.searchValue) ||
-            x.contactNumber.startsWith(this.state.searchValue))));
-        return p
-    };
-
-    changeSearchValue = (newSearchValue) => {
-       this.setState({
-           searchValue: newSearchValue
-       })
-    };
 
     render() {
         return (
             <div>
                 <Header />
-                <div className="container">
-                    <SearchHandler searchValueChanged={this.changeSearchValue}/>
-                    <ContactList contacts={this.getContacts()} />
-                    <InputHandler onAddClick={this.onAddClick}/>
-                </div>
+                <TodoContainer />
             </div>
     )
     }
--- contact-list/src/components/Header/index.js
+++ contact-list/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './styles.css'
 
@@ -18,7 +19,17 @@ class Header extends React.Component {
                                  <a className="nav-link navigation-link navigation-link-active" href="#">Home <span className="sr-only">(current)</span></a>
                              </li>
                              <li className="nav-item">
-                                 <a className="nav-link navigation-link" href="#">Features</a>
+
+                                 <a className="nav-link navigation-link" href="#">Todo</a>
+                             </li>
+                             <li className="nav-item">
+                                 <a className="nav-link navigation-link" href="#">Contacts</a>
+                             </li>
+                             <li className="nav-item">
+                                 <a className="nav-link btn-like" href="#">Log In</a>
+                             </li>
+                             <li className="nav-item">
+                                 <a className="nav-link btn-like" href="#">Register</a>
                              </li>
                          </ul>
                      </div>
